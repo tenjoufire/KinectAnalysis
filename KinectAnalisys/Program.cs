559040cc77e5c762a81d086205e58f4c8443452b
tenjoufire <tenjoufire7@gmail.com>
2017-06-08 21:32:05 +0900

    TrackingID別に注目度を出せるようにした

--- KinectAnalisys/Program.cs
+++ KinectAnalisys/Program.cs
@@ -11,32 +11,39 @@ namespace KinectAnalysis
 {
     class Program
     {
-        private static string jsonString;
-        private static int[] PYR = new int[3];
+        private static FaceInfo faceInfo;
         private static int page = 0;
         static void Main(string[] args)
         {
             while (true)
             {
                 Console.WriteLine("ようこそ 利用したい機能の数字を入力してください");
-                Console.WriteLine("[1]:単一ファイルの読み込み[0]:終了");
+                Console.WriteLine("[1]:単一ファイルの読み込み[2]:(TrackingID別)単一ファイルの読み込み[0]:終了");
                 page = Int32.Parse(Console.ReadLine());
+                string fileName;
                 switch (page)
                 {
+                    
                     case 1:
                         Console.WriteLine("ファイル名を入力してください(.jsonは不要)");
-                        var fileName = Console.ReadLine();
+                        fileName = Console.ReadLine();
                         fileName += ".json";
 
-                        jsonString = GetJsonFile(fileName);
+                        faceInfo = GetJsonObject(fileName);
 
-                        var faceInfo = JsonConvert.DeserializeObject<FaceInfo>(jsonString);
-
-                        GetAverageRotationScore(faceInfo);
+                        GetAverageRotationScore(faceInfo.faceInfos);
 
                         break;
 
                     case 2:
+                        Console.WriteLine("ファイル名を入力してください(.jsonは不要)");
+                        fileName = Console.ReadLine();
+                        fileName += ".json";
+
+                        faceInfo = GetJsonObject(fileName);
+
+                        GetAverageRotationScores();
+
 
                         break;
                     case 0:
@@ -49,9 +56,10 @@ namespace KinectAnalysis
             }
         }
 
-        private static void GetAverageRotationScore(FaceInfo faceInfo)
+        private static void GetAverageRotationScore(List<Face> faceInfos)
         {
-            foreach (var face in faceInfo.faceInfos)
+            float[] PYR = new float[3];
+            foreach (var face in faceInfos)
             {
                 PYR[0] += Math.Abs(face.pitch);
                 PYR[1] += Math.Abs(face.yaw);
@@ -64,14 +72,36 @@ namespace KinectAnalysis
                 Console.WriteLine($"[{i}] {PYR[i]}");
         }
 
-        private static string GetJsonFile(string filePath)
+        private static FaceInfo GetJsonObject(string filePath)
         {
             string json;
             using (var sr = new StreamReader(filePath))
             {
                 json = sr.ReadLine();
             }
-            return json;
+            return JsonConvert.DeserializeObject<FaceInfo>(json);
+        }
+
+        private static void GetAverageRotationScores()
+        {
+            var faceInfos = faceInfo.faceInfos;
+            List<Face>[] faceDividedID = new List<Face>[6];
+            for(int i = 0; i < faceDividedID.Length; i++)
+            {
+                if(faceInfos.Where(x => x.trackingID == i) != null)
+                {
+                    faceDividedID[i] = faceInfos.Where(x => x.trackingID == i).ToList();
+                }
+            }
+
+            Console.WriteLine("[0]:pitch [1]:yaw [2]:roll");
+
+            for(int i = 0; i < faceDividedID.Length; i++)
+            {
+                Console.WriteLine($"\nTrackingID {i}");
+                GetAverageRotationScore(faceDividedID[i]);
+            }
+            Console.ReadLine();
         }
     }
 }
