8fa50af20db51bd79c09fefb33f8eed01e6b4f1b
tenjoufire <tenjoufire7@gmail.com>
2017-06-08 11:14:04 +0900

    json読み込みの実装

--- KinectAnalisys/Program.cs
+++ KinectAnalisys/Program.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KinectWPF;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace KinectAnalysis
 {
     class Program
     {
+        private static string jsonString;
         static void Main(string[] args)
         {
+            Console.WriteLine("ファイル名を入力してください(同一ディレクトリに配置されてたら)");
+            var fileName = Console.ReadLine();
+            using(var sr = new StreamReader(fileName))
+            {
+                jsonString = sr.ReadLine();
+            }
+
+            var faceInfo = JsonConvert.DeserializeObject<FaceInfo>(jsonString);
+
+            Console.ReadLine();
         }
     }
 }
